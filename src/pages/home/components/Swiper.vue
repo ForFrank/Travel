a7dfad6d94c04ce98de7a8c1f839f6c77e0cd041
frank <liangzhenqi@hotmail.com>
2018-12-25 13:30:08 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/' +
+        'fusion/1812/d1/cd4f32e924a5a302.jpg_750x200_3d492837.jpg'
+      }, {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/' +
+        'fusion/1811/f7/72ce947f4dff4c02.jpg_750x200_aa583dba.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
