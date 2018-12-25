d95f9e614aa6d19932e54b1c969deb7d1c667f69
frank <liangzhenqi@hotmail.com>
2018-12-25 20:48:31 +0800

    add recommend and weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/' +
+        'd2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/' +
+        'd2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/' +
+        'd2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top: .2em
+    line-height: .8rem
+    background: #eee
+    text-indext: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1flex
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
